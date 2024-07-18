64015e073bb736eb79ea2718dfb8584aee87a3f4
yuatzin <ycortes432@gmail.com>
2024-07-17 23:40:45 -0600

    inicio se sesion
    cambios yuatzin inicio de sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,49 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NoIngresadoGuard } from './no-ingresado.guard';
+import { IngresadoGuard } from './ingresado.guard';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
+  
   {
     path: 'producto',
-    loadChildren: () => import('./producto/producto.module').then( m => m.ProductoPageModule)
+    loadChildren: () => import('./producto/producto.module').then( m => m.ProductoPageModule),
+    
   },
   {
     path: 'ordenes',
-    loadChildren: () => import('./ordenes/ordenes.module').then( m => m.OrdenesPageModule)
-  }
+    loadChildren: () => import('./ordenes/ordenes.module').then( m => m.OrdenesPageModule),
+    
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+    canActivate: [IngresadoGuard]
+
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule),
+    canActivate: [IngresadoGuard]
+
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule),
+    canActivate: [IngresadoGuard]
+  },
+
+
+  
 ];
 
 @NgModule({
---
+++ src/app/menu/menu-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MenuPage } from './menu.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MenuPage,
+    children: [{
+      path: 'producto',
+      loadChildren: () => import('../producto/producto.module').then( m => m.ProductoPageModule)
+    },
+    {
+      path: 'ordenes',
+      loadChildren: () => import('../ordenes/ordenes.module').then( m => m.OrdenesPageModule)
+    },
+    {
+      path: 'inicio',
+      loadChildren: () => import('../inicio/inicio.module').then( m => m.InicioPageModule)
+    }]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MenuPageRoutingModule {}
+
