5720432e1a458df3479d1ea7ca2c72166cd07413
mario david cortez <davocortez88@gmail.com>
2024-07-18 23:30:27 -0600

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,13 +23,13 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
-    canActivate: [IngresadoGuard]
+    canActivate: [NoIngresadoGuard]
 
   },
   {
     path: 'registro',
     loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule),
-    canActivate: [IngresadoGuard]
+    canActivate: [NoIngresadoGuard]
 
   },
   {
@@ -39,7 +39,7 @@ const routes: Routes = [
   {
     path: 'menu',
     loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule),
-    canActivate: [IngresadoGuard]
+    canActivate: [NoIngresadoGuard]
   },
 
 
