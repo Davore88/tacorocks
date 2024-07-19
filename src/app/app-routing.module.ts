7f0c180938ebec2f804922498d7c43ea649caef6
yuatzin <ycortes432@gmail.com>
2024-07-18 19:10:11 -0600

    inicio sesion
    cambios de yua 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,13 +23,13 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
-    canActivate: [IngresadoGuard]
+    canActivate: [NoIngresadoGuard]
 
   },
   {
     path: 'registro',
     loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule),
-    canActivate: [IngresadoGuard]
+    canActivate: [NoIngresadoGuard]
 
   },
   {
